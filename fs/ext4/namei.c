5013174935ecddfea7ae71bdc79d743b8d285010
David Howells <dhowells@redhat.com>
2017-03-31 18:31:56 +0100

    ext4: Add statx support
    Return enhanced file attributes from the Ext4 filesystem.  This includes
    the following:
    
     (1) The inode creation time (i_crtime) as stx_btime, setting STATX_BTIME.
    
     (2) Certain FS_xxx_FL flags are mapped to stx_attribute flags.
    
    This requires that all ext4 inodes have a getattr call, not just some of
    them, so to this end, split the ext4_getattr() function and only call part
    of it where appropriate.
    
    Example output:
    
    	[root@andromeda ~]# touch foo
    	[root@andromeda ~]# chattr +ai foo
    	[root@andromeda ~]# /tmp/test-statx foo
    	statx(foo) = 0
    	results=fff
    	  Size: 0               Blocks: 0          IO Block: 4096    regular file
    	Device: 08:12           Inode: 2101950     Links: 1
    	Access: (0644/-rw-r--r--)  Uid:     0   Gid:     0
    	Access: 2016-02-11 17:08:29.031795451+0000
    	Modify: 2016-02-11 17:08:29.031795451+0000
    	Change: 2016-02-11 17:11:11.987790114+0000
    	 Birth: 2016-02-11 17:08:29.031795451+0000
    	Attributes: 0000000000000030 (-------- -------- -------- -------- -------- -------- -------- --ai----)
    
    Signed-off-by: David Howells <dhowells@redhat.com>
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/ext4/namei.c
+++ fs/ext4/namei.c
@@ -3972,6 +3972,7 @@ const struct inode_operations ext4_dir_inode_operations = {
 	.tmpfile	= ext4_tmpfile,
 	.rename		= ext4_rename2,
 	.setattr	= ext4_setattr,
+	.getattr	= ext4_getattr,
 	.listxattr	= ext4_listxattr,
 	.get_acl	= ext4_get_acl,
 	.set_acl	= ext4_set_acl,
@@ -3980,6 +3981,7 @@ const struct inode_operations ext4_dir_inode_operations = {
 
 const struct inode_operations ext4_special_inode_operations = {
 	.setattr	= ext4_setattr,
+	.getattr	= ext4_getattr,
 	.listxattr	= ext4_listxattr,
 	.get_acl	= ext4_get_acl,
 	.set_acl	= ext4_set_acl,
