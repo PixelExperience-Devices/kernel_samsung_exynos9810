bd333d294b5e7df914c006c2e8af14d34c5cc379
John Dias <joaodias@google.com>
2018-06-13 22:16:28 -0700

    cpufreq: schedutil: clear cached_raw_freq when invalidated
    The cpufreq_schedutil governor keeps a cache of the last
    raw frequency that was mapped to a supported device frequency.
    If the next request for a frequency matches the cached
    value, the policy's next_freq value is reused. But there
    are paths that can update the raw cached value without
    updating the next_freq value, and there are paths that
    can set the next_freq value without setting the raw
    cached value. On those paths, the cached value
    must be reset.
    
    The case that has been observed is when a frequency request
    reaches sugov_update_commit but is then rejected by to
    the sugov_up_down_rate_limit check.
    
    Bug: 109757301
    Change-Id: I7c585339a04ff1732054d6e5b36a57e2d41266aa
    Signed-off-by: John Dias <joaodias@google.com>
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -171,7 +171,7 @@ static void sugov_update_commit(struct sugov_policy *sg_policy, u64 time,
 	struct cpufreq_policy *policy = sg_policy->policy;
 	int cpu;
 
-	if (sugov_up_down_rate_limit(sg_policy, time, next_freq))
+	if (sg_policy->next_freq == next_freq)
 		return;
 
 	if (sg_policy->next_freq == next_freq)
